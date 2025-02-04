2e90d1b624ef0928a27c9dcd049ad4b27c1723ca
Sasvidu Abesinghe <66816946+Sasvidu@users.noreply.github.com>
2025-02-04 13:35:55 +0530

    feat: metadata

--- app/clubs/layout.tsx
+++ app/clubs/layout.tsx
@@ -10,8 +10,35 @@ const outfit = Outfit({
 });
 
 export const metadata: Metadata = {
-  title: "CCSG Events",
-  description: "Upcoming events at CCSG",
+  title: "CCSG Club Events",
+  description: "Discover and participate in upcoming internal events by the Curtin Colombo Student Guild.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/ccsg.png",
+  },
+  openGraph: {
+    title: "CCSG Events",
+    description: "Stay updated on upcoming internal CCSG events and exclusive student activities.",
+    url: "https://ccsgevents.lk",
+    siteName: "CCSG Events",
+    images: [
+      {
+        url: "/ccsg.png",
+        width: 1200,
+        height: 630,
+        alt: "CCSG Events Banner",
+      },
+    ],
+    type: "website",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "CCSG Events",
+    description: "Stay updated on upcoming CCSG events and exclusive student activities.",
+    images: ["/ccsg.png"],
+  },
 };
 
 export default function RootLayout({
